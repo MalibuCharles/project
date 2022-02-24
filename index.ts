3b4a03abcc15f7d920d8c90796d84c775c88b631
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-02-24 13:11:28 -0500

    first

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { message } from "antd"
+
+interface User{
+    id: string;
+    username: string
+}
+
+interface Message {
+    id: string;
+    userId: string;
+    text: string;
+    sentAt: Date;
+}
+
+const createUser = (user: User) => {
+
+}
+
+const sendMessage = (message: Message) => {
+
+}
+
+const getMessages = (): Message[] => {
+return [];
+}
